82e26375979d04e45528adeb705d89296aeb243b
Elodie S <elodie.sevestre86@gmail.com>
2026-04-07 15:15:44 +0200

    init fetch

--- src/api.js
+++ src/api.js
@@ -1,4 +1,11 @@
 // api.js : contient fetch et construction de l'url
 
+// 1. url API
 const url = `
 /api/explore/v2.1/catalog/datasets/244400404_wifi-public-exterieur-nantes-metropole/records?limit=20&offset=0`;
+console.log(url);
+
+// 2. envoyer la requête HTTP = fetch
+// 2.1. await = "attends que le serveur répode avant de continuer"
+const reponse = await fetch(url);
+// console.log(reponse);
