4b08e28f9fe0af82d94113240ff386bddaa546f7
Elodie S <elodie.sevestre86@gmail.com>
2026-04-07 22:00:21 +0200

    display raw data

--- src/main.js
+++ src/main.js
@@ -6,4 +6,12 @@ import "./style.css";
 import { requestAPI } from "./api.js";
 // ============================== TEST ==============================
 
-requestAPI();
+const data = await requestAPI();
+// console.log(data);
+const container = document.getElementById("data");
+
+data.results.forEach((item) => {
+  const div = document.createElement("div");
+  div.innerHTML = `<h3>${item.site.replaceAll("_", " ") || "Sans nom"}</h3>`;
+  container.appendChild(div);
+});
