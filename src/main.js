f3cc16899296822870b960989e87a008cb2b4dab
Elodie S <elodie.sevestre86@gmail.com>
2026-04-08 08:23:57 +0200

    transfert main vers render

--- src/main.js
+++ src/main.js
@@ -3,15 +3,5 @@
 // ============================== IMPORT ==============================
 
 import "./style.css";
-import { requestAPI } from "./api.js";
+import "./api.js";
 // ============================== TEST ==============================
-
-const data = await requestAPI();
-// console.log(data);
-const container = document.getElementById("data");
-
-data.results.forEach((item) => {
-  const div = document.createElement("div");
-  div.innerHTML = `<h3>${item.site.replaceAll("_", " ") || "Sans nom"}</h3>`;
-  container.appendChild(div);
-});
