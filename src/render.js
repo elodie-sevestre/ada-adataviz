7618d0794e3632dfa8045d28e1939a2da16fcf75
Elodie S <elodie.sevestre86@gmail.com>
2026-04-08 08:23:57 +0200

    transfert main vers render

--- src/render.js
+++ src/render.js
@@ -1,1 +1,11 @@
 // render.js : création des cartes HTML
+
+const data = await requestAPI();
+// console.log(data);
+const container = document.getElementById("data");
+
+data.results.forEach((item) => {
+  const div = document.createElement("div");
+  div.innerHTML = `<h3>${item.site.replaceAll("_", " ") || "Sans nom"}</h3>`;
+  container.appendChild(div);
+});
