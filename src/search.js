d66b6a99293de59a4af682ed520367f239c3feef
Elodie S <elodie.sevestre86@gmail.com>
2026-04-09 12:28:45 +0200

    init search

--- src/search.js
+++ src/search.js
@@ -2,8 +2,18 @@
 
 // je récupère le bouton et l'input dans le HTML
 
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
 // quand on clique sur le bouton :
-// je lis ce qu'il y a dans l'input
-// je mets à jour query avec ce texte en minuscules
-// je remets offset à 0
-// j'appelle loading()
+
+searchButton.addEventListener("click", () => {
+  // je lis ce qu'il y a dans l'input
+  const userSearch = searchInput.value;
+  // je mets à jour query avec ce texte en minuscules
+  query = userSearch.toLowerCase();
+  // je remets offset à 0
+  offset = 0;
+  // j'appelle loading()
+  loading();
+});
