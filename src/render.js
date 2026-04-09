a468e5b0ddd28149b86128aa11f0bd479d1cce24
Elodie S <elodie.sevestre86@gmail.com>
2026-04-09 11:08:05 +0200

    voir plus voir moins

--- src/render.js
+++ src/render.js
@@ -10,15 +10,33 @@ const list = document.getElementById("born-list");
  * Crée une carte HTML pour une borne wifi et l'ajoute à la liste.
  * @param {Object} result objet représentant une borne wifi
  * @param {string} result.site nom du site
- * @param {string} result.adresse adresse de la borne
  */
 export const createCard = (result) => {
   const div = document.createElement("div");
   div.classList.add("card");
 
-  div.innerHTML = `${result.site.replaceAll("_", " ")}`;
-
+  div.innerHTML = `
+  <p>${result.site.replaceAll("_", " ")}</p>
+  <div class="details hidden">
+  <p>${result.adresse}</p>
+  <p>${result.code_postal} - ${result.commune}</p>
+  </div>
+  <button class="load-details">Voir plus</button>
+  `;
   list.appendChild(div); // insère la carte dans le DOM
+
+  const details = div.querySelector(".details");
+  const button = div.querySelector(".load-details");
+
+  button.addEventListener("click", () => {
+    details.classList.toggle("hidden"); // affiche ou cache details
+    // changer le texte du bouton
+    if (button.textContent === "Voir plus") {
+      button.textContent = "Voir moins";
+    } else {
+      button.textContent = "Voir plus";
+    }
+  });
 };
 
 // Appel à l'API (await possible ici car le fichier est chargé comme module ES)
