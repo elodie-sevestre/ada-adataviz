d3f8503fd78a50a3cee63937fbc2545cc294d0f4
Elodie S <elodie.sevestre86@gmail.com>
2026-04-10 08:05:27 +0200

    continue pagination

--- src/pagination.js
+++ src/pagination.js
@@ -1,16 +1,18 @@
 // pagination.js : logique offset / charger plus
 
 // import
-import { query, loading } from "./main.js";
+import "./main.js";
 
 // je récupère le bouton "charger plus" dans le HTML
 const moreLoadButton = document.getElementById("load-more");
 
+let currentOffset = 0;
 // quand on clique sur le bouton :
 moreLoadButton.addEventListener("click", () => {
   // j'augmente offset de 8
+  currentOffset += 8;
   // j'appelle loading() en mode "ajouter" (append = true)
-  loading(query, 8);
+  loading(query, currentOffset);
 });
 
 // render.js
