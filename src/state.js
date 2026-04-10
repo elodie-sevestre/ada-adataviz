74b08171f2d5e44c064921d35ed5f3e3b2d832f2
Elodie S <elodie.sevestre86@gmail.com>
2026-04-10 12:38:39 +0200

    clean

--- src/state.js
+++ src/state.js
@@ -2,6 +2,12 @@ export let query = "";
 export let offset = 0;
 export let totalCount = 0;
 
-export const setQuery = (value) => { query = value; };
-export const setOffset = (value) => { offset = value; };
-export const setTotalCount = (value) => { totalCount = value; };
+export const setQuery = (value) => {
+  query = value;
+};
+export const setOffset = (value) => {
+  offset = value;
+};
+export const setTotalCount = (value) => {
+  totalCount = value;
+};
